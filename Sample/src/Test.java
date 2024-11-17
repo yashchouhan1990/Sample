4891e7cb94248d449ca7ab06941e677fcaa9d8d8
Yash Chouhan <yashchauhanyash@gmail.com>
2024-11-17 17:03:50 +0530

    Initial Commit in this repo

---
+++ Sample/src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	
+	public static double average (int a, int b)
+	{
+		return a + b / 2;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println (average (2,1));
+		System.out.println("Hello");
+	}
+
+}
